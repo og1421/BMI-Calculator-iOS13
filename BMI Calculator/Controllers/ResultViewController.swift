e284341140821c3718cc0318b4b574ece96554b0
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-05-03 10:34:28 -0300

    Updating Model

--- BMI Calculator/Controllers/ResultViewController.swift
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -18,7 +18,6 @@ class ResultViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         bmiLabel.text = bmiValue
-        
     }
     
     @IBAction func recalculatePressed(_ sender: UIButton) {
